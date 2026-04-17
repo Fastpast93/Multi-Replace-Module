397e6d505e009b9013bc9995eb7c9ebfa2f17592
Fastpast93 <zzari.fastpastIO@gmail.com>
2026-04-17 03:52:36 -0500

    Add files via upload

---
+++ replace_mod.py
@@ -0,0 +1,26 @@
+#replace_mod
+def mul_replace(string, dict):
+    for key, value in dict.items():
+        string = string.replace(key, value)
+    return string
+if __name__ == '__main__':
+    print('Guide')
+    print('Define the replacement dictionary like so:')
+    print('replacement_dict= {"item1":"replacement1", "item2":"replacement2", "item3":"replacement3"}')
+    print('You can add any amount of items and replacements as you like.')
+    print('Now that you have your dictionary, you can use mul_replace like so:')
+    print('your_substituted_text = mul_replace(your_string, replacement_dict)')
+    print('The items found in your_string will be replaced by their respective value in the replacement dictionary.')
+
+#Guide
+#Define the replacement dictionary like so:
+#replacement_dict= {"item1":"replacement1", "item2":"replacement2", "item3":"replacement3"}
+#You can add any amount of items and replacements as you like.
+#Now that you have your dictionary, you can use mul_replace like so:
+#your_substituted_text = mul_replace(your_string, replacement_dict)
+#The keywords found in your_string will be replaced by their respective value in the replacement dictionary.
+#Heres an example:
+replacement_dict = {"Hello":"Hi there,", "books":"paper"}
+string = "Hello World! I like reading books and coding!"
+print(f"Original: {string}")
+print(f"Updated:  {mul_replace(string, replacement_dict)}")
